18a06e433211ede3aec96c0d22067d4844a1191d
Kirty Meena <kirtymeena@gmail.com>
2022-04-04 18:45:13 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import Dashboard from "./components/Dashboard";
 
 function App() {
+    const visitProfile = ()=>{
+      window.location.href="https://www.frontendmentor.io/profile/kirtymeena"
+    }
+    const visitWebsite=()=>{
+      window.location.href="https://www.frontendmentor.io/home"
+    }
   return (
     <div className="App">
         <Dashboard/>
+        <footer>
+        <p>Challenge by <span onClick={visitWebsite}>Frontend Mentor</span>. Coded by <span onClick={visitProfile}>Kirty Meena</span></p>.
+        </footer>
     </div>
   );
 }
